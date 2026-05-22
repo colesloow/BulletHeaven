a0870a2316ac58a434784fe6a3aac537c0ff1ccd
niloccolinus <niloccolin@protonmail.com>
2026-05-22 21:46:22 +0200

    PlayerMovement: cache Camera.main, rename private fields to camelCase

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,55 +4,58 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float _speed = 5f;
-    [SerializeField] private float _acceleration = 20f;
-    [SerializeField] private float _radius = 0.5f;
-    [SerializeField] private InputActionReference _moveAction;
-    [SerializeField] private Transform _bodyMesh;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float acceleration = 20f;
+    [SerializeField] private float radius = 0.5f;
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private Transform bodyMesh;
 
-    private Rigidbody _rigidbody;
+    private Rigidbody rb;
     private Vector3 _moveDirection;
+    private Camera mainCamera;
 
-    public void AddSpeed(float amount) => _speed += amount;
+    public void AddSpeed(float amount) => speed += amount;
 
     private void OnEnable()
     {
-        _moveAction.action.Enable();
+        moveAction.action.Enable();
     }
 
     private void OnDisable()
     {
-        _moveAction.action.Disable();
+        moveAction.action.Disable();
     }
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        Vector2 input = _moveAction.action.ReadValue<Vector2>();
+        if (mainCamera == null) mainCamera = Camera.main;
 
-        Vector3 camForward = Vector3.ProjectOnPlane(Camera.main.transform.forward, Vector3.up).normalized;
-        Vector3 camRight = Vector3.ProjectOnPlane(Camera.main.transform.right, Vector3.up).normalized;
+        Vector2 input = moveAction.action.ReadValue<Vector2>();
+
+        Vector3 camForward = Vector3.ProjectOnPlane(mainCamera.transform.forward, Vector3.up).normalized;
+        Vector3 camRight = Vector3.ProjectOnPlane(mainCamera.transform.right, Vector3.up).normalized;
         _moveDirection = (camForward * input.y + camRight * input.x).normalized;
 
         // Drive rolling from actual Rigidbody velocity so it reflects real movement
-        Vector3 velocity = _rigidbody.linearVelocity;
+        Vector3 velocity = rb.linearVelocity;
         velocity.y = 0f;
         if (velocity.sqrMagnitude > 0.01f)
         {
             float distance = velocity.magnitude * Time.deltaTime;
-            float angle = distance / _radius * Mathf.Rad2Deg;
+            float angle = distance / radius * Mathf.Rad2Deg;
             Vector3 rollAxis = Vector3.Cross(Vector3.up, velocity.normalized);
-            _bodyMesh.Rotate(rollAxis, angle, Space.World);
+            bodyMesh.Rotate(rollAxis, angle, Space.World);
         }
     }
 
     private void FixedUpdate()
     {
-        Vector3 targetVelocity = _moveDirection * _speed;
-        _rigidbody.linearVelocity = Vector3.MoveTowards(_rigidbody.linearVelocity, targetVelocity, _acceleration * Time.fixedDeltaTime);
+        Vector3 targetVelocity = _moveDirection * speed;
+        rb.linearVelocity = Vector3.MoveTowards(rb.linearVelocity, targetVelocity, acceleration * Time.fixedDeltaTime);
     }
 }
